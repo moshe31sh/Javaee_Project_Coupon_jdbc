bdbc70e37e0c92d4520bafc44fb60d23906639b0
moshe31sh <moshe31sh@gmail.com>
2015-10-24 15:25:18 +0300

    First ex coupons system data base

--- Coupon_ex/src/com/company/Main.java
+++ Coupon_ex/src/com/company/Main.java
@@ -1,17 +1,37 @@
 package com.company;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
-    public static void main(String[] args) {
 
-        Coupon myCoupon = new Coupon(2,"food", "free food for all"); //class test
-        //System.out.println(myCoupon.toString());
+    public static void main(String[] args) {
+    Coupon myCoupon = new Coupon(2,"food","Free food for all");
         try {
-            MySQLCouponsDAO.getInstance().addCoupon(myCoupon);
-        }
-        catch (CouponsPlatformException e){
+            MySQLCouponsDAO.getInstance().buildTable();
+            try {
+                MySQLCouponsDAO.getInstance().addCoupon(myCoupon);
+               Coupon[] arr = MySQLCouponsDAO.getInstance().getCoupons();
+                for (Coupon coupon : arr){
+                    System.out.println(coupon.toString());
+                }
+               if (MySQLCouponsDAO.getInstance().deleteCoupon(myCoupon) == true){
+                   System.out.println("Successfully deleted "+myCoupon.getId());
+               }
+                else{
+                   System.out.println("Delete failed "+ myCoupon.getDescription());
+               }
+            }catch (CouponsPlatformException e){
+                e.printStackTrace();
+            }
+            MySQLCouponsDAO.getInstance().deleteTable();
+        }catch (Exception e){
             e.printStackTrace();
         }
-        }
+
+    }
 }
--- Coupon_ex/src/com/company/MySQLCouponsDAO.java
+++ Coupon_ex/src/com/company/MySQLCouponsDAO.java
@@ -1,11 +1,38 @@
 package com.company;
 
+import java.sql.*;
 
 public class MySQLCouponsDAO implements ICouponsDAO {
 
     private static MySQLCouponsDAO instance;
+    public static String driver = "org.apache.derby.jdbc.ClientDriver";
+    public static String protocol = "jdbc:derby://localhost:1527/gagamoDB;create=true";
+    private  int count;
+    private Connection connection;
+    private Statement statement;
+    private ResultSet rs;
 
-    private MySQLCouponsDAO() {}
+    private MySQLCouponsDAO() {init();}
+
+    private void init(){
+         connection = null;
+         statement = null;
+         rs = null;
+        count = 0;
+        try {
+            connection = null;
+            //Instantiating the dirver class will indirectly register
+            //this driver as an available driver for DriverManager
+            Class.forName(driver);
+            //Getting a connection by calling getConnection
+            connection = DriverManager.getConnection(protocol);
+            statement = connection.createStatement();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 
     public static MySQLCouponsDAO getInstance() {
         if(instance==null) {
@@ -14,23 +41,75 @@ public class MySQLCouponsDAO implements ICouponsDAO {
         return instance;
     }
 
-
     @Override
+    /**
+     * addCoupon . Adding coupon details to data base
+     */
     public void addCoupon(Coupon coupon) throws CouponsPlatformException {
         // TODO Auto-generated method stub
 
+        try{
+            count++;
+            statement.execute("INSERT INTO Coupons VALUES (" + coupon.getId() + " ," + coupon.getTitle() + " , " + coupon.getDescription()+")");
+
+    }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
+    /**
+     * deleteCoupon. deleting coupon from data base by id
+     */
     public boolean deleteCoupon(Coupon coupon) throws CouponsPlatformException {
         // TODO Auto-generated method stub
+        try{
+            count--;
+            statement.execute("DELETE  FROM  Coupons WHERE id IN (5)");
+            return true;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public Coupon[] getCoupons() throws CouponsPlatformException {
+        try {
+
+            rs = statement.executeQuery("SELECT id,title ,description   FROM Coupons ORDER BY id");
+
+           // while(rs.next()) count++;
+            Coupon [] arrayOfCoupons = new Coupon[count];
+            int i = 0 ;
+            while(rs.next()) {
+
+            arrayOfCoupons[i] = new Coupon(rs.getInt("id") ,rs.getString("title") , rs.getString("description"));
+                i++;
+            }
+            return arrayOfCoupons;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         // TODO Auto-generated method stub
         return null;
     }
 
+    public void buildTable(){
+        try {
+            statement.execute("create table Coupons(id int, title  VARCHAR (50) NOT NULL , description VARCHAR (50) NOT NULL )");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteTable(){
+        try {
+            statement.execute("DROP TABLE Coupons");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
